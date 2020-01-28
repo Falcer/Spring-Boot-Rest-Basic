beeeac362ea013586fb3fe10dee34994b4449092
arganaphangquestian <arganaphangquestian@gmail.com>
2020-01-28 15:57:13 +0700

    Edit Delete Todo Controller

--- src/main/java/dev/falcer/restbasic/controller/TodoController.java
+++ src/main/java/dev/falcer/restbasic/controller/TodoController.java
@@ -29,7 +29,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/{id}")
-    boolean delete(@RequestParam("id") Long id) {
+    boolean delete(@PathVariable("id") Long id) {
         return todoService.delete(id);
     }
 }
